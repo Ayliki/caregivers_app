8066a3b75260f456e48e58bbf2a6a9c779600186
Ilya Belov <ilyabelov518@gmail.com>
2024-09-20 15:54:30 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './Components/LandingPage/LandingPage';
-import ContactForm from './Components/ContactForm/ContactForm';
-import Contract from './Components/Contract/Contract';
-import PrivacyPolicy from './Components/PrivacyPolicy/PrivacyPolicy';
+import { Suspense, lazy } from 'react';
+
+const LandingPage = lazy(() => import('./Components/LandingPage/LandingPage'));
+const ContactForm = lazy(() => import('./Components/ContactForm/ContactForm'));
+const Contract = lazy(() => import('./Components/Contract/Contract'));
+const PrivacyPolicy = lazy(() => import('./Components/PrivacyPolicy/PrivacyPolicy'));
 
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* Landing Page Route */}
-        <Route path="/" element={<LandingPage />} />
-
-        {/* Contact Form Route */}
-        <Route path="/contact" element={<ContactForm />} />
-
-        {/* Contract and Privacy Policy Routes */}
-        <Route path="/contract" element={<Contract />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/contact" element={<ContactForm />} />
+          <Route path="/contract" element={<Contract />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
